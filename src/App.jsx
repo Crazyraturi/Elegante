74a286040b3ad35a38822a97f290068c7ab23349
Aditya Kumar <adityaayush1712@gmail.com>
2025-11-18 17:28:26 +0530

    footer pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import Home from "./pages/Home";
 import NavBar from "./components/common/NavBar";
 import Footer from "./components/common/Footer";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
+import TrackOrder from "./pages/TrackOrder";
+import ReturnOrder from "./pages/ReturnOrder";
+import TermsandCondition from "./pages/TermsandCondition";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ShippingPolicy from "./pages/ShippingPolicy";
+import ContactUs from "./pages/Contactus";
+import FAQpage from "./pages/FAQpage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
-     
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product-details" element={<ProductDetails />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product-details" element={<ProductDetails />} />
+        <Route path="/track-order" element={<TrackOrder />} />
+        <Route path="/returns-exchange" element={<ReturnOrder />} />
+        <Route path="/terms" element={<TermsandCondition />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/shipping" element={<ShippingPolicy />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/faqs" element={<FAQpage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
